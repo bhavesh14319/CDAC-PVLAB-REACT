6fd15b109bb1b540ac475e491951228f008b30f3
Nimesh <nimesh.sakhiya2003@gmail.com>
2023-04-04 17:52:25 +0530

    changes

--- src/components/Quiz/components/Quiz.jsx
+++ src/components/Quiz/components/Quiz.jsx
@@ -1,42 +1,74 @@
-import React from 'react';
+import React from "react";
 import "../Styles/Quiz.css";
+import "../Styles/Start.css";
 
-const Quiz = ({ showQuiz, question, quizs, checkAnswer, correctAnswer, selectedAnswer, questionIndex, nextQuestion, showTheResult }) => {
+const Quiz = ({
+  showQuiz,
+  question,
+  quizs,
+  checkAnswer,
+  correctAnswer,
+  selectedAnswer,
+  questionIndex,
+  nextQuestion,
+  showTheResult,
+}) => {
+  return (
+    // <section className="bg-dark text-white"  }}>
+    <div
+      className="start_container"
+      style={{ display: `${showQuiz ? "block" : "none"}` }}
+    >
+      <div className="question_contaainer">
+        <div className="d-flex justify-content-between gap-md-3">
+          <h5 style={{ color: "#60d600", textAlign: "left", padding: "15px" }}>
+            <span>QUESTION NO. &emsp;</span>
+            <span style={{ fontSize: "large" }}>
+              {quizs.indexOf(question) + 1} / {quizs?.length}
+            </span>
+          </h5>
+          <h5 className="question_text">{question?.question}</h5>
+        </div>
+        <div className="options_main_container">
+          {question?.options?.map((item, index) => (
+            <>
+              <div className="options">
+                <span className="option_number">{index + 1}</span>
+                <button
+                  key={index}
+                  className="options_container"
+                  onClick={(event) => checkAnswer(event, item)}
+                >
+                  {item}
+                </button>
+              </div>
+            </>
+          ))}
+        </div>
 
-    return (
-        // <section className="bg-dark text-white"  }}>
-            <div className="start_container" style={{ display: `${showQuiz ? 'block' : 'none'}`}}>
-                <div className="row vh-100 align-items-center justify-content-center">
-                    <div className="col-lg-8">
-                        <div className="card p-4" style={{ background: '#3d3d3d', borderColor: '#646464' }}>
-                            <div className="d-flex justify-content-between gap-md-3">
-                                <h5 className='mb-2 fs-normal lh-base'>{question?.question}</h5>
-                                <h5 style={{ color: '#60d600', width: '100px', textAlign: 'right' }}>{quizs.indexOf(question) + 1} / {quizs?.length}</h5>
-                            </div>
-                            <div>
-                                {
-                                    question?.options?.map((item, index) => <button
-                                        key={index}
-                                        className={`option w-100 text-start btn text-white py-2 px-3 mt-3 rounded btn-dark ${correctAnswer === item && 'bg-success'}`}
-                                        onClick={(event) => checkAnswer(event, item)}
-                                    >
-                                        {item}
-                                    </button>)
-                                }
-                            </div>
-
-                            {
-                                (questionIndex + 1) !== quizs.length ?
-                                    <button className='btn py-2 w-100 mt-3 bg-primary text-light fw-bold' onClick={nextQuestion} disabled={!selectedAnswer}>Next Question</button>
-                                    :
-                                    <button className='btn py-2 w-100 mt-3 bg-primary text-light fw-bold' onClick={showTheResult} disabled={!selectedAnswer}>Show Result</button>
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
-        // </section>
-    );
+        {questionIndex + 1 !== quizs.length ? (
+          <div className="submit_button_container">
+            <button
+              className="submit_button"
+              onClick={nextQuestion}
+              disabled={!selectedAnswer}
+            >
+              Next Question
+            </button>
+          </div>
+        ) : (
+          <button
+            className="btn py-2 w-100 mt-3 bg-primary text-light fw-bold"
+            onClick={showTheResult}
+            disabled={!selectedAnswer}
+          >
+            Show Result
+          </button>
+        )}
+      </div>
+    </div>
+    // </section>
+  );
 };
 
 export default Quiz;
